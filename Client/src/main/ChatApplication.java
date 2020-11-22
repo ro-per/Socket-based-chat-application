0ddcd65d7ebe8a4c3060a6d00899fd494547bbbf
Romeo Permentier <56132002+ro-per@users.noreply.github.com>
2020-11-22 20:29:25 +0100

    minor improvements

--- Client/src/main/ChatApplication.java
+++ Client/src/main/ChatApplication.java
@@ -21,7 +21,6 @@ import java.util.logging.Logger;
 
 public class ChatApplication extends Application {
     private final Logger logger = Logger.getLogger(ChatApplication.class.getName());
-
     private static Stage publicStage, privateStage;
     private static Scene publicScene;
     private static BorderPane publicBorderPane;
